e06cdc26c41f4d88306403026984bf48e16ba5e2
Arif Sanchez <arifsanchez@gmail.com>
2011-03-24 03:10:41 -0700

    add simpletest

---
+++ app/tests/cases/controllers/users_controller.test.php
@@ -0,0 +1,47 @@
+<?php
+/* Users Test cases generated on: 2011-03-24 03:00:48 : 1300960848*/
+App::import('Controller', 'Users');
+
+class TestUsersController extends UsersController {
+	var $autoRender = false;
+
+	function redirect($url, $status = null, $exit = true) {
+		$this->redirectUrl = $url;
+	}
+}
+
+class UsersControllerTestCase extends CakeTestCase {
+	var $fixtures = array('app.user');
+
+	function startTest() {
+		$this->Users =& new TestUsersController();
+		$this->Users->constructClasses();
+	}
+
+	function endTest() {
+		unset($this->Users);
+		ClassRegistry::flush();
+	}
+
+	function testIndex() {
+
+	}
+
+	function testView() {
+
+	}
+
+	function testAdd() {
+
+	}
+
+	function testEdit() {
+
+	}
+
+	function testDelete() {
+
+	}
+
+}
+?>
